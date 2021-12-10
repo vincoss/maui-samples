df14de1cc810e6b92f803e12de11d1e7cba00a52
fero <feroalien@hotmail.com>
2021-12-10 13:52:28 +1000

    file picker sample

--- src/FilePicker_Samples/MainPage.xaml.cs
+++ src/FilePicker_Samples/MainPage.xaml.cs
@@ -1,24 +1,56 @@
 ﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FilePicker_Samples
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnPicker(object sender, EventArgs e)
         {
-            count++;
-            CounterLabel.Text = $"Current count: {count}";
+            var filePath = await PickFileAsync(null);
+            lblInfo.Text = filePath;
+        }
 
-            SemanticScreenReader.Announce(CounterLabel.Text);
+        public async Task<string> PickFileAsync(string[] allowedTypes = null)
+        {
+            if (allowedTypes == null)
+            {
+                allowedTypes = new string[0];
+            }
+
+            var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.Android, allowedTypes },
+                { DevicePlatform.iOS, allowedTypes },
+                { DevicePlatform.macOS, allowedTypes },
+                { DevicePlatform.tvOS, allowedTypes },
+                { DevicePlatform.Tizen, allowedTypes },
+                { DevicePlatform.UWP, allowedTypes },
+                { DevicePlatform.watchOS, allowedTypes },
+                { DevicePlatform.Unknown, allowedTypes },
+            });
+
+            var options = new PickOptions
+            {
+                PickerTitle = "Pick a file",
+                FileTypes = customFileType
+            };
+
+            var result = await FilePicker.PickAsync(options);
+
+            if (result != null)
+            {
+                return result.FullPath;
+            }
+            return null;
         }
     }
 }
