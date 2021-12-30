87e59280ac673eac9de62c7389e9753e9e7fb048
Ferdinand <feroalien@hotmail.com>
2021-12-30 17:00:52 +1000

    swipe view sample - not working

---
+++ src/SwipeView_Samples/Views/UI_SwipeViewMenuView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace SwipeView_Samples.Views
+{
+    public partial class UI_SwipeViewMenuView : ContentPage
+    {
+        public UI_SwipeViewMenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            swipeView.Open(OpenSwipeItem.RightItems);
+        }
+    }
+}
