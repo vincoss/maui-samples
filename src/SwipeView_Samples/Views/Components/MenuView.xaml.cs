a826762132ed95f090ef2fede866aeb4322d6a60
fero <feroalien@hotmail.com>
2022-01-18 08:25:12 +1000

    swipeview sample updated

---
+++ src/SwipeView_Samples/Views/Components/MenuView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace SwipeView_Samples.Views.Components
+{
+    public partial class MenuView
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
