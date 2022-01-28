93157b2beb611e8f961878e78ec32ceba11e2f57
fero <feroalien@hotmail.com>
2022-01-28 14:30:25 +1000

    Frame sample

---
+++ src/Frame_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Frame_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Frame_Samples/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Frame_Samples
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
