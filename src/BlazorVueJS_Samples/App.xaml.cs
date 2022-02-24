e7a540ab002c275d1c810a48a34f26bd7227159d
Ferdinand <feroalien@hotmail.com>
2022-02-25 07:31:49 +1000

    update not found

--- src/BlazorVueJS_Samples/App.xaml.cs
+++ src/BlazorVueJS_Samples/App.xaml.cs
@@ -5,6 +5,19 @@
         public App()
         {
             InitializeComponent();
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            if(ex == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(ex);
         }
 
         protected override Window CreateWindow(IActivationState activationState)
