134c86599da4df9ff5a08048a895b332f0038b29
fero <feroalien@hotmail.com>
2022-05-17 16:08:01 +1000

    new project

---
+++ src/MauiBlazorApp_VueJS_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazorApp_VueJS_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/MauiBlazorApp_VueJS_Samples/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorApp_VueJS_Samples
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
