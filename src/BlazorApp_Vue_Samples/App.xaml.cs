36590ca805801a257c49e66efef814a71d5f3979
Ferdinand <feroalien@hotmail.com>
2022-06-02 13:34:43 +1000

    blazor app template

---
+++ src/BlazorApp_Vue_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp_Vue_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
