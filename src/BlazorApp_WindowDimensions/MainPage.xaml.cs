4a96f04653ab0b0b8c16c341315950f79d8d760e
fero <feroalien@hotmail.com>
2022-06-13 14:48:47 +1000

    get dimensions

---
+++ src/BlazorApp_WindowDimensions/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp_WindowDimensions
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/BlazorApp_WindowDimensions/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorApp_WindowDimensions
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
