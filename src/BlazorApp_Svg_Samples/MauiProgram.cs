d7e7dfd3e4027497e5a653f2ed2e7d4b17b6ba96
Ferdinand <feroalien@hotmail.com>
2022-07-01 08:50:53 +1000

    svg blazor samples

---
+++ src/BlazorApp_Svg_Samples/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp_Svg_Samples.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace BlazorApp_Svg_Samples
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("Sea-Life.otf", "SeaLife");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
