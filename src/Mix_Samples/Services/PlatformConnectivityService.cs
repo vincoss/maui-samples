77fd181afa14863576c256791405f79a7cf33078
fero <feroalien@hotmail.com>
2022-07-08 11:07:21 +1000

    connection sevice

---
+++ src/Mix_Samples/Services/PlatformConnectivityService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix_Samples.Services
+{
+    public interface IConnectivityService
+    {
+        bool IsConnected { get; }
+    }
+
+    public class PlatformConnectivityService : IConnectivityService
+    {
+        private bool HasInternetConnection()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsConnected
+        {
+            get { return HasInternetConnection(); }
+        }
+    }
+}
