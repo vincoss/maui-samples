f588ed9a7ac49c12ed9a30c43c9b227774196d10
fero <feroalien@hotmail.com>
2022-07-26 16:23:14 +1000

    android web authenticator

--- src/OidcSamples/Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ src/OidcSamples/Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -1,14 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 
-namespace OidcSamples.Platforms.Android
+namespace OidcSamples.Platforms
 {
-    //[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+    //[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
     //[IntentFilter(new[] { Android.Content.Intent.ActionView },
-    //          Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
-    //          MainPage.CallbackUri)]
-    //public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+    //     Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+    //     DataScheme = MainPage.CallbackUri)]
+    //public class WebAuthenticationCallbackActivity : Microsoft.Maui.WebAuthenticatorCallbackActivity
     //{
-
     //}
+
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+              Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+              DataScheme = MainPage.CallbackUri)]
+    public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+    {
+
+    }
 }
