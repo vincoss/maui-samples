bc543f168f5a35c497797c88641c9e650ba51c06
fero <feroalien@hotmail.com>
2022-08-02 14:38:11 +1000

    new project

---
+++ src/Setup_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Setup_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Setup_Samples/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Setup_Samples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
