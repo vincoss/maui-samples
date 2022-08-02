315ef1f3901e007201601644fce2db6c53c6499f
fero <feroalien@hotmail.com>
2022-08-02 13:08:30 +1000

    initial splash

---
+++ src/Splash_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Splash_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
