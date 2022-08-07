23f48f78d03087fbbc5e4720128f533092d5e17a
fero <feroalien@hotmail.com>
2022-08-08 09:22:47 +1000

    OAuth project initial

---
+++ src/OAuth_Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OAuth_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
