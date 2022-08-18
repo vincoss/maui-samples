1d95e88da69ce83e87ac609748b947e93f9241b0
fero <feroalien@hotmail.com>
2022-08-18 13:09:34 +1000

    catch all exceptions

--- src/Logging_Startup_Samples/App.xaml.cs
+++ src/Logging_Startup_Samples/App.xaml.cs
@@ -15,7 +15,13 @@ namespace Logging_Startup_Samples
             SetupLogging();
 
             _logger = InbuiltLog.For(typeof(App));
+            _logger.Debug($"Constructor - {nameof(App)}");
+
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            _logger.Debug($"Constructor - {nameof(App)}");
 
             MainPage = new AppShell();
         }
@@ -45,6 +51,16 @@ namespace Logging_Startup_Samples
             InbuiltLog.SetFactory(factory);
         }
 
+        private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                return;
+            }
+            var exception = new Exception("AppDomain_CurrentDomain_FirstChanceException", e.Exception);
+            LogException(exception);
+        }
+
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             var ex = e.ExceptionObject as Exception;
@@ -52,8 +68,23 @@ namespace Logging_Startup_Samples
             {
                 return;
             }
+            var exception = new Exception("AppDomain_CurrentDomain_UnhandledException", ex);
+            LogException(exception);
+        }
 
-            _logger.Error(ex, ex.Message);
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                return;
+            }
+            var exception = new Exception("TaskScheduler_UnobservedTaskException", e.Exception);
+            LogException(exception);
+        }
+
+        private void LogException(Exception exception)
+        {
+            _logger.Error(exception, exception.Message);
         }
     }
 }
