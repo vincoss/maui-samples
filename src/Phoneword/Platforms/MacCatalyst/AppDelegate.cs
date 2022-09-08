92b4685c32582bd8c346e841f4fea66fdc8520ab
fero <feroalien@hotmail.com>
2022-09-09 09:06:08 +1000

    new project tutorial

---
+++ src/Phoneword/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Phoneword
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
