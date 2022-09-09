8f4f5c7035418ca5b4b7304a66d860ec74e84223
fero <feroalien@hotmail.com>
2022-09-09 10:00:47 +1000

    phone translator tutorial

--- src/Phoneword/MainPage.xaml.cs
+++ src/Phoneword/MainPage.xaml.cs
@@ -1,24 +1,56 @@
-﻿namespace Phoneword
+﻿using Phoneword.Services;
+
+namespace Phoneword
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        string translatedNumber;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnTranslate(object sender, EventArgs e)
         {
-            count += 2;
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+       "Dial a Number",
+       "Would you like to call " + translatedNumber + "?",
+       "Yes",
+       "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
         }
     }
 }
