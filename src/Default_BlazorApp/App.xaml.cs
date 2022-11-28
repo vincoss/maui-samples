c236576de5082bfa959692dd836038e279dfa900
fero <feroalien@hotmail.com>
2022-11-29 09:49:32 +1000

    default app

---
+++ src/Default_BlazorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Default_BlazorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
