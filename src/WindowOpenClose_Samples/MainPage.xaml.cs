eb7489bf1e6947a1d0bf6fd5a741b90f1813d50e
fero <feroalien@hotmail.com>
2023-01-27 12:08:08 +1000

    windows close

--- src/WindowOpenClose_Samples/MainPage.xaml.cs
+++ src/WindowOpenClose_Samples/MainPage.xaml.cs
@@ -12,10 +12,20 @@ namespace WindowOpenClose_Samples
         private void Button_Clicked(object sender, EventArgs e)
         {
             Window secondWindow = new Window(new PageOne());
+
+            // Size and position
             secondWindow.Width = 300;
             secondWindow.Height = 300;
             secondWindow.X = 50;
             secondWindow.Y = 50;
+
+            // OR
+            // Get display size
+            //var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+
+            //// Center the window
+            //Window.X = (displayInfo.Width / displayInfo.Density - Window.Width) / 2;
+            //Window.Y = (displayInfo.Height / displayInfo.Density - Window.Height) / 2;
 
             Application.Current.OpenWindow(secondWindow);
         }
