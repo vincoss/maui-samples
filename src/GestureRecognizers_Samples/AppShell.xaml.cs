01da4f7f0a5a08869e95eed67e19ff9a82d758f9
fero <feroalien@hotmail.com>
2023-01-31 11:03:01 +1000

    Pointer gesture samples

---
+++ src/GestureRecognizers_Samples/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestureRecognizers_Samples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
