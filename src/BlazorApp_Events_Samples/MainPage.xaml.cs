eba9e2a7e0785badf4a92d20f4d953e0e5ba7ee5
fero <feroalien@hotmail.com>
2023-06-09 14:02:32 +1000

    blazor app samples

---
+++ src/BlazorApp_Events_Samples/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorApp_Events_Samples
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainPage_Loaded;
+            this.Unloaded += MainPage_Unloaded;
+            this.Focused += MainPage_Focused;
+            this.NavigatingFrom += MainPage_NavigatingFrom;
+            this.NavigatedTo += MainPage_NavigatedTo;
+        }
+
+        private void MainPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+        {
+        }
+        private void MainPage_NavigatingFrom(object sender, NavigatingFromEventArgs e)
+        {
+        }
+
+        private void MainPage_Focused(object sender, FocusEventArgs e)
+        {
+        }
+
+        private void MainPage_Unloaded(object sender, EventArgs e)
+        {
+        }
+
+        private void MainPage_Loaded(object sender, EventArgs e)
+        {
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
