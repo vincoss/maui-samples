73c40a6301d3913d2efd0d7a878b99a9b19ef9bb
fero <feroalien@hotmail.com>
2023-09-15 12:13:43 +1000

    project added

---
+++ src/Blazor_AppWithWebServer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_AppWithWebServer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
