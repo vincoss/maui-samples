99e2bb3b737bb03c21e26e25baacf731d724a1ce
fero <feroalien@hotmail.com>
2023-09-15 15:19:25 +1000

    server

---
+++ src/Blazor_AppWithWebServer/WebServerStartup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Blazor_AppWithWebServer
+{
+    public class WebServerStartup
+    {
+        public WebServerStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           // MvcServiceCollectionExtensions
+          //  services.AddControllers();
+
+            ConfigDependencies(services);
+        }
+
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddControllers();
+
+        //    ConfigDependencies(services);
+        //}
+
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IEndpointRouteBuilder endpoits)
+        //{
+        //    endpoits.MapControllers();
+        //}
+
+        public void ConfigDependencies(IServiceCollection services)
+        {
+        }
+    }
+}
