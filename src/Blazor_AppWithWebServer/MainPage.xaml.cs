4632017214a4ed74f5ce5ca745245646bfb1e9b6
fero <feroalien@hotmail.com>
2023-09-15 10:07:12 +1000

    new project

---
+++ src/Blazor_AppWithWebServer/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_AppWithWebServer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Blazor_AppWithWebServer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Blazor_AppWithWebServer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
