1ebe1b85c4367cc53d574788529a16c7933d5afd
fero <feroalien@hotmail.com>
2023-10-11 11:38:07 +1000

    blazor local web server

--- src/Blazor_AppWithWebServer_EmbedIO/Services/ServerHostingExtensions.cs
+++ src/Blazor_AppWithWebServer_EmbedIO/Services/ServerHostingExtensions.cs
@@ -2,6 +2,9 @@
 using EmbedIO;
 using EmbedIO.WebApi;
 using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Reflection;
 using System.Threading;
 
@@ -34,18 +37,32 @@ namespace EmbedIO_ServerConsole
 
         public static string GetUrl()
         {
+            var ip = GetLocalIp();
+
             int _port = 5167;
             string _ip = "http://localhost:";
 
             var platform = DeviceInfo.Platform;
 
-            if (platform == DevicePlatform.Android)
-            {
-                _ip = _ip.Replace("localhost", "10.0.2.2");
-            }
+            //if (platform == DevicePlatform.Android)
+            //{
+                _ip = _ip.Replace("localhost", ip.ToString());
+            //}
 
             string Url = $"{_ip}{_port}";
             return Url;
+        }
+
+        private static string GetLocalIp()
+        {
+            string localIP;
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                var endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
         }
     }
 }
