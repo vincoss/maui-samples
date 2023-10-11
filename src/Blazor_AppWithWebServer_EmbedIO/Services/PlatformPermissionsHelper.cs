570fb642a9d9b3ac67b4725d9aebdc4549fa6de2
fero <feroalien@hotmail.com>
2023-10-12 09:53:56 +1000

    permissions check

---
+++ src/Blazor_AppWithWebServer_EmbedIO/Services/PlatformPermissionsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace Blazor_AppWithWebServer_EmbedIO.Services
+{
+    /// <summary>
+    /// var status = await PermissionsHelper.CheckAndRequestPermissionAsync<Permissions.LocationWhenInUse>();
+    /// if (status != PermissionStatus.Granted)
+    /// {
+    ///    //show message
+    /// }
+    /// </summary>
+    public static class PlatformPermissionsHelper
+    {
+        public static class PermissionsHelper
+        {
+            public static async Task<PermissionStatus> CheckAndRequestPermissionAsync<TPermission>()
+                where TPermission : BasePermission, new()
+            {
+                return await MainThread.InvokeOnMainThreadAsync(async () =>
+                {
+                    TPermission permission = new TPermission();
+                    var status = await permission.CheckStatusAsync();
+                    if (status != PermissionStatus.Granted)
+                    {
+                        status = await permission.RequestAsync();
+                    }
+
+                    return status;
+                });
+            }
+        }
+    }
+}
