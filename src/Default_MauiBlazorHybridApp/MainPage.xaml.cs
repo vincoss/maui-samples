83ce63680fb015ed8a879b39b84687b0f5581eb5
fero <feroalien@hotmail.com>
2025-08-07 10:18:24 +1000

    project update

---
+++ src/Default_MauiBlazorHybridApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Default_MauiBlazorHybridApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
