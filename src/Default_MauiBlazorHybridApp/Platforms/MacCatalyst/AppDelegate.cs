721518cefd4b5f51c74a132c7a7b7aafeafe8061
fero <feroalien@hotmail.com>
2026-08-06 16:03:35 +1000

    samples update

---
+++ src/Default_MauiBlazorHybridApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Default_MauiBlazorHybridApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
